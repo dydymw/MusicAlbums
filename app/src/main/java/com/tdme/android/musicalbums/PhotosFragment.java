0d0e077077a1f3a3ea24f463e0ef0fb321f6b734
Tdme <1092648985@qq.com>
2017-09-18 11:54:58 +0800

    commit

--- app/src/main/java/com/tdme/android/musicalbums/PhotosFragment.java
+++ app/src/main/java/com/tdme/android/musicalbums/PhotosFragment.java
@@ -19,9 +19,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.tdme.android.musicalbums.activity.MainActivity;
 import com.tdme.android.musicalbums.adapter.PhotoNetAdapter;
 import com.tdme.android.musicalbums.bean.ListEvent;
 import com.zhihu.matisse.Matisse;
